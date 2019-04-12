d36f5a015b4ecb6bb5c6f2678e0bcaab068b0ec4
James Benson <jjbenson85@gmail.com>
2019-04-12 17:35:24 +0100

    Get select elements with jQuery

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log('Hello')
+
+//Get Dom Elements
+
+const $selSupplier = $('#selSupplier')
+const $selProduct = $('#selProduct')
+
+
+//Get supplier
+$selSupplier.on('change', (e)=> console.log(e.target.value))
+
+//Update Product List
+handleSuplierChange(){
+  
+}
+//Display Product details

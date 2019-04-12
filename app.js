e1c94cd84ddb27a56cbbf0da352abd5c5f37ba7f
James Benson <jjbenson85@gmail.com>
2019-04-12 18:26:59 +0100

    Displays product details when product selected

--- app.js
+++ app.js
@@ -1,31 +1,79 @@
 console.log('Hello')
 
-//Get Dom Elements
 
+//Globals
+let selectedSupplier
+let selectedProductId
+
+//Get Dom Elements
 const $selSupplier = $('#selSupplier')
 const $selProduct = $('#selProduct')
+const $tbodyProduct = $('#tbodyProduct')
 
-const oldCo = ['Small wongle', 'Large wongle', 'Super wongle']
-const newCo = ['Mini Wongle', 'Small Wongle', 'Large Wongle', 'Super Wongle']
-const productArr = {oldCo, newCo}
+
+const productArr = [
+  {
+    name: 'Small wongle',
+    price: 5,
+    supplier: 'New Co Ltd.',
+    id: 0
+  },
+  {
+    name: 'Large wongle',
+    price: 8,
+    supplier: 'New Co Ltd.',
+    id: 1
+  },
+  {
+    name: 'Super wongle',
+    price: 12,
+    supplier: 'New Co Ltd.',
+    id: 2
+  },
+  {
+    name: 'Mini wongle',
+    price: 4,
+    supplier: 'Old Co Ltd.',
+    id: 3
+  },
+  {
+    name: 'Small wongle',
+    price: 6,
+    supplier: 'Old Co Ltd.',
+    id: 4
+  },
+  {
+    name: 'Large wongle',
+    price: 9,
+    supplier: 'Old Co Ltd.',
+    id: 5
+  },
+  {
+    name: 'Super wongle',
+    price: 13,
+    supplier: 'Old Co Ltd.',
+    id: 6
+  }
+]
 
 
 //Get supplier
 $selSupplier.on('change', handleSuplierChange)
 $selProduct.on('change', handleProductChange)
 
+
 //Update Product List
 function handleSuplierChange(e){
 
   console.log(e.target.value)
-  const supplier = e.target.value
-  const products = productArr[supplier]
+  selectedSupplier = e.target.value
+  const products = productArr.filter(product => product.supplier === e.target.value)
 
   //Clear Select Element
   $selProduct.empty()
 
   //Create options for Select Element
-  const output = products.map(product => $(`<option>${product}</option>`))
+  const output = products.map(product => $(`<option value=${product.id}>${product.name}</option>`))
 
   //Add Options to Select element
   $selProduct.append(output)
@@ -34,4 +82,22 @@ function handleSuplierChange(e){
 //Display Product details
 function handleProductChange(e){
   console.log(e.target.value)
+  selectedProductId = e.target.value
+
+  const product = productArr.find( product => product.id === parseInt(selectedProductId) )
+  console.log(product, productArr, parseInt(selectedProductId) )
+
+  //Clear Table
+  $tbodyProduct.empty()
+
+  //Create cells for Select Element
+  const output = `<tr>
+      <th>${product.id}</th>
+      <th>${product.supplier}</th>
+      <th>${product.name}</th>
+      <th>${product.price}</th>
+  </tr>`
+
+  //Append table row to table
+  $tbodyProduct.append(output)
 }

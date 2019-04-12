dc46e391ecf5b4dbfe5987541b94cf4f6908fb0f
James Benson <jjbenson85@gmail.com>
2019-04-12 17:55:49 +0100

    Product list created dynamically

--- app.js
+++ app.js
@@ -5,12 +5,33 @@ console.log('Hello')
 const $selSupplier = $('#selSupplier')
 const $selProduct = $('#selProduct')
 
+const oldCo = ['Small wongle', 'Large wongle', 'Super wongle']
+const newCo = ['Mini Wongle', 'Small Wongle', 'Large Wongle', 'Super Wongle']
+const productArr = {oldCo, newCo}
+
 
 //Get supplier
-$selSupplier.on('change', (e)=> console.log(e.target.value))
+$selSupplier.on('change', handleSuplierChange)
+$selProduct.on('change', handleProductChange)
 
 //Update Product List
-handleSuplierChange(){
-  
+function handleSuplierChange(e){
+
+  console.log(e.target.value)
+  const supplier = e.target.value
+  const products = productArr[supplier]
+
+  //Clear Select Element
+  $selProduct.empty()
+
+  //Create options for Select Element
+  const output = products.map(product => $(`<option>${product}</option>`))
+
+  //Add Options to Select element
+  $selProduct.append(output)
 }
+
 //Display Product details
+function handleProductChange(e){
+  console.log(e.target.value)
+}
